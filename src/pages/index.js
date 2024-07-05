135895b632436cdad6e837747ea8082dedbbbc04
brayd-1573 <98786062+brayd-1573@users.noreply.github.com>
2024-07-05 18:36:05 -0500

    test charts

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,8 @@ import {
 } from "@mui/material";
 import { spacing } from "@mui/system";
 
+import Stats from "../../components/pages/dashboard/default/Stats";
+
 const Divider = styled(MuiDivider)(spacing);
 const Typography = styled(MuiTypography)(spacing);
 
@@ -17,7 +19,7 @@ function Index() {
     <React.Fragment>
       <Helmet title="Portfolio Simulator" />
       <Typography
-        variant="h6"
+        variant="h5"
         gutterBottom
         display="inline"
         align="center"
@@ -27,11 +29,47 @@ function Index() {
       </Typography>
       <Divider my={6} />
       <Grid container spacing={6}>
-        {/* components here */}
+        <Grid item xs={12} sm={12} md={6} lg={3} xl>
+          <Stats
+            title="Sales Today"
+            amount="2.532"
+            chip="Today"
+            percentagetext="+26%"
+            percentagecolor={green[500]}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl>
+          <Stats
+            title="Visitors"
+            amount="170.212"
+            chip="Annual"
+            percentagetext="-14%"
+            percentagecolor={red[500]}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl>
+          <Stats
+            title="Total Earnings"
+            amount="$ 24.300"
+            chip="Monthly"
+            percentagetext="+18%"
+            percentagecolor={green[500]}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={3} xl>
+          <Stats
+            title="Pending Orders"
+            amount="45"
+            chip="Yearly"
+            percentagetext="-9%"
+            percentagecolor={red[500]}
+            illustration="/static/img/illustrations/waiting.png"
+          />
+        </Grid>
       </Grid>
       <Divider my={6} />
       <Typography
-        variant="h6"
+        variant="h5"
         gutterBottom
         display="inline"
         align="center"
