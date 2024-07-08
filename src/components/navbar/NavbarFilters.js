d50ba373f49d7e2ddaec58ede47130c58cb8c389
ethanc7 <98353307+ethanc7@users.noreply.github.com>
2024-07-08 15:06:55 -0500

    test

--- src/components/navbar/NavbarFilters.js
+++ src/components/navbar/NavbarFilters.js
@@ -6,7 +6,7 @@ function NavbarFilters() {
   return (
     <React.Fragment>
       <Typography variant="subtitle1" color="textPrimary">
-        Portfolio Breakdown, Simulation Filters and Start
+        Portfolio Breakdown, Simulation Filters and Start yo mama
       </Typography>
     </React.Fragment>
   );
