ec99bd3280334e88fcbd77099c4ffdafe9e3a8e2
brayd-1573 <98786062+brayd-1573@users.noreply.github.com>
2024-07-10 09:44:36 -0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "@emotion/styled";
 import { Helmet } from "react-helmet-async";
 import DashboardLayout from "../layouts/Dashboard";
@@ -11,22 +11,14 @@ import { spacing } from "@mui/system";
 
 import StrategyResults from "./simulation_results/strategy";
 import PortfolioResults from "./simulation_results/portfolio";
-import Navbar from "../components/navbar/Navbar";
 
 const Divider = styled(MuiDivider)(spacing);
 const Typography = styled(MuiTypography)(spacing);
 
 function Index() {
-  const [simulationData, setSimulationData] = useState({});
-
-  const handleSimulationRun = (data) => {
-    setSimulationData(data);
-  };
-
   return (
     <React.Fragment>
       <Helmet title="Portfolio Simulator" />
-      <Navbar onSimulationRun={handleSimulationRun} />
       <Typography
         variant="h2"
         gutterBottom
@@ -37,7 +29,7 @@ function Index() {
         Strategy Performance
       </Typography>
       <Divider my={6} />
-      <StrategyResults data={simulationData} />
+      <StrategyResults />
       <Typography
         variant="h2"
         gutterBottom
@@ -48,7 +40,7 @@ function Index() {
         Portfolio Results
       </Typography>
       <Divider my={6} />
-      <PortfolioResults data={simulationData} />
+      <PortfolioResults />
     </React.Fragment>
   );
 }
